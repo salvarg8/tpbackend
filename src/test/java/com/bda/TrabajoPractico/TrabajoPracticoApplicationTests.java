9891c176b1edba246fad1fede7f4cf47ea47a18d
Salvador Rodriguez Garraza <salvarodriguez_91@hotmail.com>
2023-11-11 22:44:05 -0300

    primer push

---
+++ src/test/java/com/bda/TrabajoPractico/TrabajoPracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bda.TrabajoPractico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoPracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
